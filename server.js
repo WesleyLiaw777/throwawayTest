f712a5a4f6e6c39a88aa4aa9e9e15e19b25ec755
WesleyLiaw <wesleyliaw777@gmail.com>
2023-09-01 14:18:17 -0500

    test working

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const { DefaultAzureCredential } = require("@azure/identity");
-const { KeyClient } = require("@azure/keyvault-keys");
+// const { DefaultAzureCredential } = require("@azure/identity");
+// const { KeyClient } = require("@azure/keyvault-keys");
 
-async function getKey() {
-  const credential = new DefaultAzureCredential();
-  const vaultUrl = "supertestkeyvault.privatelink.vault.azure.net";
-  const client = new KeyClient(vaultUrl, credential);
+// async function getKey() {
+//   const credential = new DefaultAzureCredential();
+//   const vaultUrl = "supertestkeyvault.privatelink.vault.azure.net";
+//   const client = new KeyClient(vaultUrl, credential);
 
-  try {
-    const key = await client.getKey("testKey");
-    console.log(`Your key is: ${key.key}`);
-  } catch (error) {
-    console.error(error);
-  }
-}
+//   try {
+//     const key = await client.getKey("testKey");
+//     console.log(`Your key is: ${key.key}`);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-getKey();
+// getKey();
+
+console.log('Hello world!');
