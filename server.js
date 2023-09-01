02eeb3b334d331b7d322061433b88853dad02af9
WesleyLiaw <wesleyliaw777@gmail.com>
2023-09-01 12:55:25 -0500

    key vault test

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
-const express = require("express");
-const azureStorage = require("azure-storage");
+const { DefaultAzureCredential } = require("@azure/identity");
+const { KeyClient } = require("@azure/keyvault-keys");
 
-const app = express();
-const port = 3000;
+async function getKey() {
+  const credential = new DefaultAzureCredential();
+  const vaultUrl = "supertestkeyvault.vault.azure.net";
+  const client = new KeyClient(vaultUrl, credential);
 
-// Initialize blob service with your storage account and key
-const blobService = azureStorage.createBlobService(
-  "devstorageapptestacct",
-  "z8P2uHspJD5RwBmAclleQ0x7x8sgmBojAxf2qNRGqb5VkBFHN/9W3nUtr+FsbkPFZfpSE9QNhUAP+ASt+ISBCQ=="
-);
+  try {
+    const key = await client.getKey("testKey");
+    console.log(`Your key is: ${key.key}`);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-app.get("/", async (req, res) => {
-  // Reference to the container and blob
-  const containerName = "testcontainer";
-  const blobName = "supertest.txt";
-
-  blobService.getBlobToText(
-    containerName,
-    blobName,
-    function (error, blobContent, blob) {
-      if (!error) {
-        res.send(`Blob content: ${blobContent}`);
-      } else {
-        res.send(`An error occurred: ${error}`);
-      }
-    }
-  );
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+getKey();
